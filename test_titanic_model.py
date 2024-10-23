8f1d10d04302010e59d23bc98f51bb4deb6a2539
A-PachecoT <andre.pacheco.t@uni.pe>
2024-10-23 08:08:45 -0500

    feat: first test

--- test_titanic_model.py
+++ test_titanic_model.py
@@ -6,21 +6,6 @@ from titanic_model import TitanicModel
 def titanic_model():
     return TitanicModel()
 
-def test_load_and_preprocess_data(titanic_model):
-    # Arrange
-    expected_feature_count = 7
-    expected_train_size = 0.8
-    expected_test_size = 0.2
-
-    # Act
-    X_train, X_test, y_train, y_test = titanic_model.load_and_preprocess_data('titanic.csv')
-
-    # Assert
-    assert X_train.shape[1] == expected_feature_count
-    assert X_test.shape[1] == expected_feature_count
-    assert len(y_train) / (len(y_train) + len(y_test)) == pytest.approx(expected_train_size, abs=0.01)
-    assert len(y_test) / (len(y_train) + len(y_test)) == pytest.approx(expected_test_size, abs=0.01)
-
 def test_train_and_validate(titanic_model):
     # Arrange
     X_train, X_test, y_train, y_test = titanic_model.load_and_preprocess_data('titanic.csv')
@@ -45,19 +30,3 @@ def test_evaluate(titanic_model):
     assert precision >= 0.75
     assert recall >= 0.75
     assert f1 >= 0.75
-
-def test_save_and_load_model(titanic_model, tmp_path):
-    # Arrange
-    X_train, X_test, y_train, y_test = titanic_model.load_and_preprocess_data('titanic.csv')
-    titanic_model.train_and_validate(X_train, y_train)
-    model_path = tmp_path / "test_model.joblib"
-
-    # Act
-    titanic_model.save_model(model_path)
-    loaded_model = TitanicModel()
-    loaded_model.load_model(model_path)
-
-    # Assert
-    original_predictions = titanic_model.model.predict(X_test)
-    loaded_predictions = loaded_model.model.predict(X_test)
-    assert np.array_equal(original_predictions, loaded_predictions)
